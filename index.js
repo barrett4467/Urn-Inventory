935a00ef24a1baf25deab76220ab1e48d7e67e72
barrett4467 <barrett4467@gmail.com>
2021-06-10 21:42:52 -0600

    set up get route

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require("express");
-const bodyParser = require("body-parser");
-const MongoClient = require("mongodb").MongoClient;
-const app = express();
+var express = require('Express');
+var app = express();
+const dotenv = require("dotenv");
+const mongoose = require('mongoose');
+
 
 
 
@@ -10,6 +11,8 @@ let PORT = 3000;
 require("dotenv");
 
 // middleware
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
 // app.use(bodyParser.urlencoded({ extended: true }));
@@ -21,45 +24,12 @@ app.listen(PORT, function(){
     console.log(`Server.js listening on ${PORT}`)
 })
 app.get("/", function(req, res){
-    res.send("hello");
+    res.render("inventory.ejs");
 })
 
-    .then(client => { 
-        console.log("Connected to Mongodb")
-        const db = client.db("Inventory")
-        const urnsCollection = db.collection("urns")
-        app.post("/urns", (req,res) => {
-            console.log(req.body);
-            urnsCollection.insertOne(req.body)
-            .then(result => {
-                res.redirect("/")
-            })
-            .catch(error => console.log(error))
-        })
-        
-        // create
-        app.get("/", function(req, res){
-            res.send("hello from server");
-            // db.collection("urns").find().toArray()
-            // .then(results => {
-            //     res.render("index.ejs", {urns: results})
-            // })
-            // .catch(error => console.error(error))
-        })
-        // update
-})
-.catch(err => {
-    console.log(err);
-})
+// POST METHOD
+app.post("/", (req, res) => {
+    console.log(req.body);
+});
 
 
-
-
-
-
-
-//upright or horizontal
-//type of wood
-//type of box (plain, inlay)
-
-// BigurnCO2021
