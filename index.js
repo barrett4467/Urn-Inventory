8d23cfa29b6eae587631e283c203190348f084fe
barrett4467 <barrett4467@gmail.com>
2021-06-10 20:19:45 -0600

    Cleaning up and reorganizing files

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const MongoClient = require("mongodb").MongoClient;
 const app = express();
 
-const MongoClient = require("mongodb").MongoClient;
+
 
 let PORT = 3000; 
 
+require("dotenv");
+
+// middleware
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
+
+// connecting public folder for update
+app.use(express.static("public"));
 
 app.listen(PORT, function(){
     console.log(`Server.js listening on ${PORT}`)
 })
+// app.get("/", function(req, res){
+//     res.send("hello");
+// })
 
 MongoClient.connect('mongodb+srv://BigUrn:BigUrn2021@cluster0.eb4nl.mongodb.net/Inventory?retryWrites=true&w=majority', {useUnifiedTopology: true})
 
@@ -28,14 +38,20 @@ MongoClient.connect('mongodb+srv://BigUrn:BigUrn2021@cluster0.eb4nl.mongodb.net/
             })
             .catch(error => console.log(error))
         })
+        
+        // create
         app.get("/", function(req, res){
-            db.collection("urns").find().toArray()
-            .then(results => {
-                console.log(results)
-                res.render("index.ejs", {urns: results})
-            })
-            .catch(error => console.error(error))
+            res.send("hello from server");
+            // db.collection("urns").find().toArray()
+            // .then(results => {
+            //     res.render("index.ejs", {urns: results})
+            // })
+            // .catch(error => console.error(error))
         })
+        // update
+})
+.catch(err => {
+    console.log(err);
 })
 
 
